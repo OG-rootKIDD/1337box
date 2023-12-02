c94dd4f95f7fad0738fdd7a67c39786b8049dbe0
rootKIDD <5319583+OG-rootKIDD@users.noreply.github.com>
2023-12-02 21:35:29 +0100

    Update rootkidd_install.sh
    editions

--- rootkidd_install.sh
+++ rootkidd_install.sh
@@ -9,14 +9,13 @@ sudo apt update && sudo apt upgrade
 sudo apt install nala curl -y
 sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
 echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
-sudo nala upgrade
+
+sudo apt update
 sudo nala install mullvad-vpn xorg xauth net-tools npm nodejs vim rust kitty firefox-esr bspwm sxhkd polybar picom rofi dunst nitrogen i3lock redshift cmus ranger nmap wpasupplicant -y
-sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup component add rust-src
-rustup component add rust-analyzer
 git clone https://github.com/radareorg/radare2
 bash ~/radare2/sys/install.sh
 
+sudo apt update
 # install vim plug-ins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -30,3 +29,10 @@ echo "rootkidd is installing configs"
 cp -r ./.config ~/.config
 cp ./.profile ~/.profile
 cp -r ./Pictures/Backgrounds ~/Pictures/Backgrounds
+
+sudo apt update
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup component add rust-src
+rustup component add rust-analyzer
+
+sudo nala upgrade
