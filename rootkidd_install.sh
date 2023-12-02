87dc5269b81f5c2575cc9e70b924ea571743605c
rootKIDD <5319583+OG-rootKIDD@users.noreply.github.com>
2023-12-02 20:27:22 +0100

    Update rootkidd_install.sh
    optimized install order

--- rootkidd_install.sh
+++ rootkidd_install.sh
@@ -4,14 +4,12 @@
 # 	Debian install with no desktop environment
 # 	User created and added to sudo
 
-echo "rootkidd is installing nala package manager"
 sudo apt update && sudo apt upgrade
-sudo apt install nala -y
+sudo apt install nala curl git -y
+sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
 sudo nala upgrade
-echo "rootkidd is installing selection of packages for system setup"
-sudo nala install xorg xauth curl git net-tools npm nodejs vim rust kitty firefox-esr bspwm sxhkd polybar picom rofi dunst nitrogen i3lock redshift cmus ranger nmap wpasupplicant -y
-# install rust-analyzer
-echo "rootkidd is installing rust-analyzer"
+sudo nala install mullvad-vpn xorg xauth net-tools npm nodejs vim rust kitty firefox-esr bspwm sxhkd polybar picom rofi dunst nitrogen i3lock redshift cmus ranger nmap wpasupplicant -y
 rustup component add rust-src
 rustup component add rust-analyzer
 
@@ -32,12 +30,6 @@ cp ./.profile ~/.profile
 echo "rootkidd is installing backgrounds"
 cp -r ./Pictures/Backgrounds ~/Pictures/Backgrounds
 
-# install mullvad vpn
-echo "rootkidd is installing mullvad-vpn"
-sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
-echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
-sudo nala upgrade
-sudo nala install mullvad-vpn
 # install radare2
 echo "rootkidd is installing radare2"
 git clone https://github.com/radareorg/radare2
