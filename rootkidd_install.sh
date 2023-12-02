32d7a6bbc8f85ba23df599e683b1fcef302a7e63
rootKIDD <5319583+OG-rootKIDD@users.noreply.github.com>
2023-12-02 20:55:01 +0100

    Update rootkidd_install.sh
    added rust install

--- rootkidd_install.sh
+++ rootkidd_install.sh
@@ -1,38 +1,32 @@
-#! /bin/bash/sh
+#!/bin/bash/sh
 # rootkidd's linux install script
 # pre-reqs: 
 # 	Debian install with no desktop environment
 # 	User created and added to sudo
 
+# install base programs
 sudo apt update && sudo apt upgrade
 sudo apt install nala curl -y
 sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
 echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
 sudo nala upgrade
 sudo nala install mullvad-vpn xorg xauth net-tools npm nodejs vim rust kitty firefox-esr bspwm sxhkd polybar picom rofi dunst nitrogen i3lock redshift cmus ranger nmap wpasupplicant -y
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup component add rust-src
 rustup component add rust-analyzer
+git clone https://github.com/radareorg/radare2
+bash ~/radare2/sys/install.sh
 
 # install vim plug-ins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# setup config scripts
+# setup configs and images
 echo "rootkidd is installing bash mods"
 cp ./.bashrc ~/.bashrc
 echo "rootkidd is installing polybar launch script"
 cp -r ./bin ~/bin
 echo "rootkidd is installing configs"
 cp -r ./.config ~/.config
-# setup profile script
 cp ./.profile ~/.profile
-# copy backgrounds
-echo "rootkidd is installing backgrounds"
 cp -r ./Pictures/Backgrounds ~/Pictures/Backgrounds
-
-# install radare2
-echo "rootkidd is installing radare2"
-git clone https://github.com/radareorg/radare2
-bash ~/radare2/sys/install.sh
-
-echo "rootkidd is done installing"
