cce0566c56fb11cd10f3b545148ad8f0908f185a
rootkidd <r@musn.eu>
2023-12-03 23:27:30 +0100

    added static .vim/autoload/plug.vim

--- rootkidd_install.sh
+++ rootkidd_install.sh
@@ -4,11 +4,19 @@
 # 	Debian install with no desktop environment
 # 	User created and added to sudo
 
-# setup configs and images
+# configs
 cp /home/rootkidd/1337box/.bashrc /home/rootkidd/.bashrc
 sudo chown rootkidd /home/rootkidd/.bashrc
 cp /home/rootkidd/1337box/.profile /home/rootkidd/.profile
 sudo chown rootkidd /home/rootkidd/.profile
+mkdir /home/rootkidd/.vim
+sudo chown rootkidd /home/rootkidd/.vim
+mkdir /home/rootkidd/.vim/autoload
+sudo chown rootkidd /home/rootkidd/.vim/autoload
+cp /home/rootkidd/1337box/.vim/autoload/plug.vim /home/rootkidd/.vim/autoload/plug.vim
+sudo chown rootkidd /home/rootkidd/.vim/autoload/plug.vim
+
+# Pictures
 mkdir /home/rootkidd/Pictures
 sudo chown rootkidd /home/rootkidd/Pictures
 mkdir /home/rootkidd/Pictures/Backgrounds
@@ -21,7 +29,7 @@ sudo chown -R /home/rootkidd/bin
 cp -r /home/rootkidd/1337box/.config /home/rootkidd/.config
 sudo chown rootkidd /home/rootkidd/.config
 
-# install base programs
+# Programs
 sudo apt install nala  -y
 sudo apt update && sudo apt upgrade
 sudo nala upgrade
@@ -35,7 +43,7 @@ rustup component add rust-analyzer
 git clone https://github.com/radareorg/radare2
 bash /home/rootkidd/radare2/sys/install.sh
 
+# update and reboot
 sudo apt update && sudo apt upgrade
 sudo nala upgrade
-
 sudo reboot
